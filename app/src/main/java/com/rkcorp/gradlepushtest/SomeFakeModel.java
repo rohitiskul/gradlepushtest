a7837e8ad35491a3ab1050bdc8de4e87b889df0b
rohitiskul <rohitiskul@gmail.com>
2014-09-01 13:48:23 +0530

    Push test initial commit

---
+++ app/src/main/java/com/rkcorp/gradlepushtest/SomeFakeModel.java
@@ -0,0 +1,9 @@
+package com.rkcorp.gradlepushtest;
+
+/**
+ * Created by rohit.k on 9/1/2014.
+ * To test gradle push just a simple model.
+ */
+public class SomeFakeModel {
+    public String id;
+}
